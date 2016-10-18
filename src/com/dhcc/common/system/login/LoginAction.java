53f53dc31806ce15bdb015b130f167275adafd52
twr <441833060@qq.com>
2016-10-18 10:19:56 +0800

    用户登陆数据中心放入session

--- src/com/dhcc/common/system/login/LoginAction.java
+++ src/com/dhcc/common/system/login/LoginAction.java
@@ -1,5 +1,6 @@
 package com.dhcc.common.system.login;
 
+import com.dhcc.bussiness.sxydidc.datacenter.DataCenterDao;
 import com.dhcc.common.system.systemLog.SaveLog;
 import com.dhcc.common.util.CreateDate;
 import com.dhcc.modal.system.Tsuser;
@@ -7,7 +8,7 @@ import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
 /**
- * 鐢ㄦ埛鐧婚檰鐨勬牎楠孉ction
+ * 用户登陆的校验Action
  * @author GYR
  *
  */
@@ -17,19 +18,19 @@ public class LoginAction  extends ActionSupport{
 	public String userid	 = "";
 	public String password	 = "";
 	public String verifycode = "";
-	public String msg		 = "";	//鐧婚檰澶辫触鐨勪俊鎭�
+	public String msg		 = "";	//登陆失败的信息
 	private String idforuser;
 	
 	/**
-	 * 鏌ヨ璇ョ敤鎴峰悕銆佸瘑鐮佹槸鍚︽纭�
-	 * @return 鏄惁鏈夋敼鐢ㄦ埛
+	 * 查询该用户名、密码是否正确
+	 * @return 是否有改用户
 	 */
 	public String QueryUser(){
 		String sVerifycode = (String) ActionContext.getContext().getSession().get("vCode");
 		if((this.verifycode != null && !this.verifycode.trim().equals("")) && (sVerifycode != null && !sVerifycode.trim().equals(""))) {
 			if(this.verifycode.equalsIgnoreCase(sVerifycode)) {
 				LoginDao dao = new LoginDao();
-				//DataCenterDao dcDao=new DataCenterDao();
+				DataCenterDao dcDao=new DataCenterDao();
 				Tsuser model = new Tsuser();
 				model = dao.QueryUser(this.userid, this.password);
 				if(null != model) {
@@ -42,10 +43,12 @@ public class LoginAction  extends ActionSupport{
 					ActionContext.getContext().getSession().put("roleIds",dao.QueryUserRoleId(model.getId()));
 					ActionContext.getContext().getSession().put("topcorpid",model.getTopcorpid());
 					
+					ActionContext.getContext().getSession().put("dcid",model.getDcid());
+					ActionContext.getContext().getSession().put("dc",dcDao.queryDataCenterById(model.getDcid()));
 					
 					ActionContext.getContext().getSession().put("corpid",dao.QueryUserCorpid(model.getId()));
 					SaveLog saveLog = new SaveLog();
-					saveLog.saveLog("鐢ㄦ埛鐧诲綍", "鐢ㄦ埛浜�"+CreateDate.getDateString()+" 鐧诲綍绯荤粺锛�");
+					saveLog.saveLog("用户登录", "用户于"+CreateDate.getDateString()+" 登录系统！");
 					saveLog = null;
 				} else {
 					this.setMsg("LOGINERROR");
